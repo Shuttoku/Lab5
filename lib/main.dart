8ba0937ef12babd5cba7208c1c75f53459364498
Shuttoku <108716387+Shuttoku@users.noreply.github.com>
2022-08-04 14:03:27 +0700

    up1

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.lime,
       ),
       home: const MyHomePage(title: 'แอพบัญชี'),
@@ -23,8 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  
 
   final String title;
 
@@ -37,13 +33,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(
-        
-        title: Text(widget.title),
-      ),
-      body: Container()
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context){
+                  return Text("Screen 2");
+                }));
+
+              }
+            )
+          ],
+        ),
+        body: Container());
   }
 }
---
+++ lib/sceens/form_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatelessWidget 
+{
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("หน้าสอง"),
+        ),
+        body: Container());
+}
+}
